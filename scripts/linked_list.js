e62e6613c6a34951986cd4e473178145b05e793f
garciabrayan <brayan.garcia02@uptc.edu.co>
2022-03-01 17:48:00 -0500

    Update linked_list.js

--- scripts/linked_list.js
+++ scripts/linked_list.js
@@ -1,6 +1,4 @@
-var head; // head of list
-
-/* Linked list Node */
+var head;
 class Node {
     constructor(val) {
         this.data = val;
@@ -8,19 +6,12 @@ class Node {
     }
 }
 
-/**
- *  Inserts a new Node at front of the list.
- */
+
 function push(new_data) {
-    /*
-    * 1 & 2: Allocate the Node & Put in the data
-    */
     var new_node = new Node(new_data);
 
-    /* 3. Make next of new Node as head */
     new_node.next = head;
 
-    /* 4. Move the head to point to new Node */
     head = new_node;
 }
 
@@ -40,11 +31,6 @@ function insertAfter(prev_node , new_data) {
     prev_node.next = new_node;
 }
 
-/*
-* Appends a new node at the end.
-This method is defined inside LinkedList class
-* shown above
-*/
 function append(new_data) {
 
     var new_node = new Node(new_data);
@@ -67,42 +53,31 @@ function append(new_data) {
     return;
 }
 
-/*
-* Given a key, deletes the first occurrence of key in linked list
-*/
 function deleteNode(key) {
     // Store head node
     var temp = head, prev = null;
 
-    // If head node itself holds the key to be deleted
     if (temp != null && temp.data == key) {
-        head = temp.next; // Changed head
+        head = temp.next; 
         return;
     }
 
-    // Search for the key to be deleted, keep track of
-    // the previous node as we need to change temp.next
     while (temp != null && temp.data != key) {
         prev = temp;
         temp = temp.next;
     }
 
-    // If key was not present in linked list
     if (temp == null)
         return;
 
-    // Unlink the node from linked list
     prev.next = temp.next;
 }
 function removeElement(element){
     var current = this.head;
     var prev = null;
 
-    // iterate over the list
+
     while (current != null) {
-        // comparing element with current
-        // element if found then remove the
-        // and return true
         if (current.element === element) {
             if (prev == null) {
                 this.head = current.next;
@@ -116,15 +91,9 @@ function removeElement(element){
     }
 
 }
-push("Daniel")
-push("Andres")
-push("Santiago")
-push("Jhoi")
-push("Luky")
-/*
-* This function prints contents of linked list
-starting from the given node
-*/
+push("garcia")
+push("brayan")
+
 function printList() {
     var tnode = head;
     var phrase = "";
@@ -140,10 +109,7 @@ function indexOf(element){
     var count = 0;
     var current = this.head;
 
-    // iterate over the list
     while (current != null) {
-        // compare each element of the list
-        // with given element
         if (current.element == element){
             console.log(count)
             return count;
@@ -153,7 +119,7 @@ function indexOf(element){
         }
     }
 
-    // not found
+
     return -1;
 }
 
@@ -165,34 +131,26 @@ function removeFrom(index){
         curr = this.head;
         prev = curr;
 
-        // deleting first element
+
         if (index === 0) {
             this.head = curr.next;
         } else {
-            // iterate over the list to the
-            // position to removce an element
+
             while (it < index) {
                 it++;
                 prev = curr;
                 curr = curr.next;
             }
 
-            // remove the element
+
             prev.next = curr.next;
         }
         this.size--;
 
-        // return the remove element
         return curr.element;
     }
 }
-/**function deleting(idName) {
-    console.log("hi")
-    removeElement(idName);
-    document.getElementById("list").innerText = "";
-    printList();
 
-}*/
 function deleting(idName) {
     console.log("hi")
     removeFrom(indexOf(idName));
@@ -206,5 +164,5 @@ function pushing(){
     document.getElementById("list").innerText = "";
 
 }
-//Problemas al eleminar un elemento de la lista al darle click
+
 window.onload = () => printList();
